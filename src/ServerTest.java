14281a387c9d7a51c3923c517b1bcda5bc46dffc
mjschuetze102 <pieisawesome102@gmail.com>
2016-12-10 21:44:25 -0500

    Created ServerTest.java

---
+++ src/ServerTest.java
@@ -0,0 +1,14 @@
+/**
+ *
+ * Created by Michael on 12/10/2016.
+ */
+
+import javax.swing.JFrame;
+
+public class ServerTest {
+    public static void main(String[] args){
+        Server server = new Server();
+        server.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        server.run();
+    }
+}
