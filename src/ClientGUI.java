978866d641dce27763bc06f21a3070f51b61bd58
mjschuetze102 <pieisawesome102@gmail.com>
2016-12-12 20:47:07 -0500

    Rearranged code into separate functions for setting dimension and margins for the GUI

--- src/ClientGUI.java
+++ src/ClientGUI.java
@@ -22,7 +22,7 @@ public class ClientGUI extends Application {
     private Client client;
 
     /** A List representing the list of users */
-    private List<String> users;
+    //private List<String> users;
 
     /** The name of the client */
     private String clientName;
@@ -54,20 +54,15 @@ public class ClientGUI extends Application {
         ObservableList<String> names = FXCollections.observableArrayList(
                 "Julia", "Ian", "Sue", "Matthew", "Hannah", "Stephan", "Denise");
         ListView<String> users = new ListView<>(names);
-        users.setPrefHeight(350);
-        users.setPrefWidth(200);
         users.setPadding(new Insets(10));
         users.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
         right.getChildren().add(users);
-        VBox.setMargin(users, new Insets(10, 10, 0, 10));
 
         // Button in the bottom right
         // When clicked will deselect all of the names from the ListView
         Button deselect= new Button("Deselect");
         deselect.setOnAction(event -> {} );
-        deselect.setPrefWidth(200);
         right.getChildren().add(deselect);
-        VBox.setMargin(deselect, new Insets(0, 10, 10, 10));
 
         // Add VBox to the BorderPane
         border.setRight(right);
@@ -82,30 +77,21 @@ public class ClientGUI extends Application {
         // TextArea in the top left
         // Displays messages
         TextArea messageArea = new TextArea();
-        messageArea.setPrefHeight(340);
-        messageArea.setPrefWidth(475);
         left.getChildren().add(messageArea);
-        VBox.setMargin(messageArea, new Insets(10, 0, 10, 10));
 
         /** HBox to line up TextField and Button */
         Pane bottom= new HBox();
-        VBox.setMargin(bottom, new Insets(0, 0, 10, 10));
 
         // TextField in the bottom left
         // Displays messages
         TextField textInput= new TextField();
-        textInput.setPrefWidth(375);
         bottom.getChildren().add(textInput);
-        HBox.setMargin(textInput, new Insets(0, 10, 0, 0));
-
 
         // Button in the bottom left
         // When clicked will send messages to the system
         Button send= new Button("Send");
         send.setOnAction(event -> {} );
-        send.setPrefWidth(80);
         bottom.getChildren().add(send);
-        HBox.setMargin(send, new Insets(0, 0, 0, 10));
 
         /** Add HBox to the VBox */
         left.getChildren().add(bottom);
@@ -115,10 +101,59 @@ public class ClientGUI extends Application {
 
 
 
+        /** Calls to the functions that change the fields from default values */
+        setDimensions(users, deselect, messageArea, textInput, send);
+        setMargins(users, deselect, messageArea, bottom, textInput, send);
+
         /** Stage Properties */
         stage.setTitle("Simple Messaging System");
         stage.setScene(new Scene(border));
         stage.show();
+    }
+
+    /**
+     * Handles all of the heights and widths of the various fields on our BorderPane
+     * @param users- ListView
+     * @param deselect- Button
+     * @param messageArea- TextArea
+     * @param textInput- TextField
+     * @param send- Button
+     */
+    private void setDimensions(ListView<String> users, Button deselect, TextArea messageArea,
+                                  TextField textInput, Button send){
+        users.setPrefHeight(350);
+        users.setPrefWidth(200);
+
+        deselect.setPrefWidth(200);
+
+        messageArea.setPrefHeight(340);
+        messageArea.setPrefWidth(475);
+
+        textInput.setPrefWidth(375);
+
+        send.setPrefWidth(80);
+    }
+
+    /**
+     * Handles all of the margins of the various fields on our BorderPane
+     * @param users- ListView
+     * @param deselect- Button
+     * @param messageArea- TextArea
+     * @param bottom- HBox
+     * @param textInput- TextField
+     * @param send- Button
+     */
+    private void setMargins(ListView<String> users, Button deselect, TextArea messageArea,
+                            Pane bottom, TextField textInput, Button send){
+        // Sets the margin for the right side of screen
+        VBox.setMargin(users, new Insets(10, 10, 0, 10));
+        VBox.setMargin(deselect, new Insets(0, 10, 10, 10));
+
+        // Sets the margin for the left side of screen
+        VBox.setMargin(messageArea, new Insets(10, 0, 10, 10));
+        VBox.setMargin(bottom, new Insets(0, 0, 10, 10));
+        HBox.setMargin(textInput, new Insets(0, 10, 0, 0));
+        HBox.setMargin(send, new Insets(0, 0, 0, 10));
     }
 
     public static void main(String[] args){
